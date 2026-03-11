8a16ca2980043bb4dddc25adc38fbfc6680fa0bd
Nikhil <gnikhilcom@gmail.com>
2026-03-11 10:08:01 +0530

    Added MongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const http = require('http');
+const https = require('https');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://admin:Admin0207@confluxaa.iaafful.mongodb.net/test")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from AWS CI/CD Pipeline!');
+  res.end('Hello from AWS CI/CD Pipeline with MongoDB!');
 });
 
 server.listen(3000, () => {
